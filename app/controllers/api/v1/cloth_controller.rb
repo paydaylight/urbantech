f7a76b3d0c1a341ce24d2f3dd43f0c9986d2fc6d
paydaylight <maxat235@gmail.com>
2019-11-02 03:25:29 +0600

    update upload

--- app/controllers/api/v1/cloth_controller.rb
+++ app/controllers/api/v1/cloth_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::ClothController < ApplicationController
   def process
-    Picture.create(file: params[:file])
-    render json: { status: 200, message: 'OK' }
+    pic = Picture.create(file: params[:file])
+    
+    if pic.valid?
+      render json: { status: 200, message: 'OK' }
+    else
+      render json: { status: 400, message: pic.errors.full_messages.first }
+    end
   end
 end
