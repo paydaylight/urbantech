b98aae62bae4e1d0291239dc412bd2ade84a285e
paydaylight <maxat235@gmail.com>
2019-11-02 15:13:47 +0600

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,7 @@ set :puma_preload_app,        true
 set :puma_worker_timeout,     nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 set :bundle_binstubs,         nil
+set :rvm_map_bins, %w{gem rake ruby rails bundle}
 # set :delayed_job_workers,     2
 # set :delayed_job_pid_dir,     '/tmp'
 # set :delayed_job_queue,       :default
